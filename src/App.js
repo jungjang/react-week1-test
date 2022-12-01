ad653fd65f9e261e23f228353ed31df5f9c6e5a2
정창원 <8500jcw@gmail.com>
2022-12-01 11:16:46 +0900

    todolist 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [goal, setGoal] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      goal: "react를 배워봅시다.",
+    },
+    {
+      id: 2,
+      goal: "useState를 배워봅시다",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="header">
+        <input
+          className="input-box"
+          type="text"
+          value={goal}
+          onChange={(e) => {
+            setGoal(e.target.value);
+          }}
+          placeholder="목표를 입력해주세요"
+        />
+        <button
+          className="add-button"
+          onClick={() => {
+            setTodos([...todos, { id: todos.length + 1, goal: goal }]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          추가하기
+        </button>
+        <br />
+        <h1>Todo List</h1>
+      </div>
+      <div className="todo-box">
+        {todos.map((todo) => (
+          <div className="todo-card" key={todo.id}>
+            {todo.goal}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
